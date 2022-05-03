fbce0a777fb7577bd22a07c51d88e17730021743
Nishant Kishore <ynm2@cdc.gov>
2022-05-03 09:49:07 -0400

    :sparkle: load specs function

---
+++ polis_api_etl-package.R
@@ -0,0 +1,3 @@
+#' @importFrom yaml read_yaml
+#' @import tidyverse
+#' @import here
---
+++ utils.R
@@ -0,0 +1,11 @@
+#Utility functions for the POLIS API ETL
+
+#' Load authorizations and local config 
+#' @param file A string 
+#' @return auth/config object
+load_specs <- function(file){
+  
+}
+ 
+#' Check to see if cache exists, if not create it
+#' 

e65fbfb3a933095ed74c5a545a84f79a972f928f
Luke Baertlein <luke.baertlein.oldsmumn@gmail.com>
2022-06-15 08:56:18 -0400

    Revert "Merge branch 'main' into 9-feature-clean-polis-data"
    This reverts commit 73097e66557d7542ae770d3cca7e4f7c73c4431f, reversing
    changes made to 7999e546416e3b76f7bbb792a6c058d383a39127.

--- R/sandbox.R
+++ R/sandbox.R
@@ -60,14 +60,17 @@ get_polis_table(table_name = "Lqas",
                 id_vars = "Id",
                 download_size = 1000)
 
+get_polis_table()
 
-get_polis_data(folder="C:/Users/wxf7/Desktop/POLIS_data",
-                token="BRfIZj%2fI9B3MwdWKtLzG%2bkpEHdJA31u5cB2TjsCFZDdMZqsUPNrgiKBhPv3CeYRg4wrJKTv6MP9UidsGE9iIDmaOs%2bGZU3CP5ZjZnaBNbS0uiHWWhK8Now3%2bAYfjxkuU1fLiC2ypS6m8Jy1vxWZlskiPyk6S9IV2ZFOFYkKXMIw%3d",
-                dev=TRUE)
+get_polis_table(folder="C:/Users/wxf7/Desktop/POLIS_data",
+                token="BRfIZj%2fI9B3MwdWKtLzG%2bkpEHdJA31u5cB2TjsCFZDdMZqsUPNrgiKBhPv3CeYRg4wrJKTv6MP9UidsGE9iIDmaOs%2bGZU3CP5ZjZnaBNbS0uiHWWhK8Now3%2bAYfjxkuU1fLiC2ypS6m8Jy1vxWZlskiPyk6S9IV2ZFOFYkKXMIw%3d")
 
 get_polis_data()
 get_polis_data(folder="C:/Users/wxf7/Desktop/POLIS_data",
                dev = TRUE)
 
-revert_from_archive(last_good_date = Sys.Date())
-revert_from_archive()
+
+test <- clean_polis_data(EnvSample)
+
+
+

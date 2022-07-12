ab6b553e4456e07de363546e0babfa358062a631
Luke Baertlein <luke.baertlein.oldsmumn@gmail.com>
2022-07-12 10:51:58 -0400

    removed duplicated functions from prior branch merge and metadata comparison debugging
    compare_final_to_archive() and save_change_summary() were defined twice. Removed duplicate.
    
    added 'verbose' to metadata_comparison() so that warning messages are only printed once per table pull

--- R/utils.R
+++ R/utils.R
@@ -337,7 +337,8 @@ get_polis_metadata <- function(query_output,
 
 #Compare metadata of newly pulled dataset to cached metadata
 metadata_comparison <- function(new_table_metadata,
-                             old_table_metadata){
+                             old_table_metadata,
+                             verbose=FALSE){
   #if new or old metadata are null, go to end
   if(!is.null(new_table_metadata) & !is.null(old_table_metadata)){
     if(nrow(new_table_metadata) != 0 & nrow(old_table_metadata) != 0){
@@ -350,7 +351,9 @@ metadata_comparison <- function(new_table_metadata,
 
     if(length(new_vars) != 0){
       new_vars <- new_vars
+      if(verbose == TRUE){
       warning(print("There are new variables in the POLIS table\ncompared to when it was last retrieved\nReview in 'new_vars'"))
+      }
     }
 
     lost_vars <- (compare_metadata %>%
@@ -358,7 +361,9 @@ metadata_comparison <- function(new_table_metadata,
 
     if(length(lost_vars) != 0){
       lost_vars <- lost_vars
+      if(verbose == TRUE){
       warning(print("There are missing variables in the POLIS table\ncompared to when it was last retrieved\nReview in 'lost_vars'"))
+      }
     }
 
     class_changed_vars <- compare_metadata %>%
@@ -372,9 +377,10 @@ metadata_comparison <- function(new_table_metadata,
 
     if(nrow(class_changed_vars) != 0){
       class_changed_vars <- class_changed_vars
+      if(verbose == TRUE){
       warning(print("There are variables in the POLIS table with different classes\ncompared to when it was last retrieved\nReview in 'class_changed_vars'"))
+      }
     }
-
     #Check for new responses in categorical variables (excluding new variables and class changed variables that have been previously shown)
     new_response <- compare_metadata %>%
       filter(!(var_name %in% lost_vars) &
@@ -393,7 +399,10 @@ metadata_comparison <- function(new_table_metadata,
 
     if(nrow(new_response) != 0){
       new_response <- new_response
-      warning(print("There are categorical responses in the new table\nthat were not seen when it was last retrieved\nReview in 'new_response'"))
+      
+      if(verbose == TRUE){
+        warning(print("There are categorical responses in the new table\nthat were not seen when it was last retrieved\nReview in 'new_response'"))
+      }
     }
 
     #Create an indicator that is TRUE if there has been a change in table structure or content that requires re-pulling of the table
@@ -1336,142 +1345,6 @@ cleaning_var_names_from_file <- function(table_name = NULL,
       rename({{new_name}} := {{original_name}})
   }
   return(input_dataframe)
-}
-
-compare_final_to_archive <- function(table_name,
-                                     id_vars,
-                                     categorical_max = 30){
-  id_vars <- as.vector(id_vars)
-  
-  #if archive exists, then compare final to archive. If not, go to end;
-  if(file.exists(paste0(load_specs()$polis_data_folder,"\\archive\\", table_name)) &
-     length(list.files(paste0(paste0(load_specs()$polis_data_folder,"\\archive\\", table_name)))) > 0){
-    #Load new_file
-    new_file <- readRDS(paste0(load_specs()$polis_data_folder, "/", table_name, ".rds"))
-    
-    #load latest file in archive subfolder
-    archive_subfolder <- paste0(load_specs()$polis_data_folder,"\\archive\\", table_name)
-    
-    #for each item in subfolder_list, get all file names then subset to most recent
-      subfolder_files <- list.files(paste0(archive_subfolder))
-      file_dates <- c()
-      for(j in subfolder_files){
-        file_date <- attr(readRDS(paste0(archive_subfolder, "\\", j)),which="updated")
-        file_dates <- c(file_dates, file_date)
-      }
-      latest_file <- c()
-      if(length(file_dates)>0){
-      latest_file <- (bind_cols(name = subfolder_files, create_date = file_dates) %>%
-                         mutate(create_date = as.POSIXct(create_date, origin = lubridate::origin)) %>%
-                         arrange(desc(create_date)) %>%
-                         slice(1))$name
-      }
-      change_summary <- NULL
-      if(length(latest_file) > 0){
-        #load latest_file
-        latest_file <- readRDS(paste0(archive_subfolder, "\\", latest_file))
-  
-        #get metadata for latest file and new_file
-        new_file_metadata <- get_polis_metadata(query_output = new_file,
-                           table_name = table_name,
-                           categorical_max = categorical_max)
-        old_file_metadata <- get_polis_metadata(query_output = latest_file,
-                                                table_name = table_name,
-                                                categorical_max = categorical_max)
-  
-        change_summary <- metadata_comparison(new_file_metadata, old_file_metadata)[2:5]
-
-        #count obs added to new_file and get set
-        in_new_not_old <- new_file %>%
-         anti_join(latest_file, by=as.vector(id_vars))
-
-        #count obs removed from old_file and get set
-        in_old_not_new <- latest_file %>%
-         anti_join(new_file, by=as.vector(id_vars))
-
-        #count obs modified in new file compared to old and get set
-          in_new_and_old_but_modified <- new_file %>%
-            inner_join(latest_file, by=as.vector(id_vars)) %>%
-            #restrict to cols in new and old
-            select(id_vars, paste0(colnames(new_file %>% select(-id_vars)), ".x"), paste0(colnames(new_file %>% select(-id_vars)), ".y")) %>%
-            #wide_to_long
-            pivot_longer(cols=-id_vars) %>%
-            mutate(source = ifelse(str_sub(name, -2) == ".x", "new", "old")) %>%
-            mutate(name = str_sub(name, 1, -3)) %>%
-            #long_to_wide
-            pivot_wider(names_from=source, values_from=value) %>%
-            filter(new != old)
-        
-        #summary counts
-          n_added <- nrow(in_new_not_old)
-          n_edited <- nrow(in_new_and_old_but_modified %>%
-                        select(id_vars) %>%
-                        unique())
-          n_deleted <- nrow(in_old_not_new)
-          obs_change <- c(n_added = n_added,
-                          n_edited = n_edited, 
-                          n_deleted = n_deleted)
-          
-        #add summary to change_summary along with datasets
-        change_summary <- append(change_summary, 
-                               list(obs_change = obs_change, 
-                                    obs_added = in_new_not_old,
-                                    obs_edited = in_new_and_old_but_modified,
-                                    obs_deleted = in_old_not_new))
-      
-    }
-    return(change_summary)
-  }
-  #If archived file does not exist, then return NULL change_summary
-  if(file.exists(paste0(load_specs()$polis_data_folder,"\\archive\\", table_name)) == FALSE){
-    return(NULL)
-  }
-}
-
-
-save_change_summary <- function(table_name, 
-                                change_summary = NULL,
-                                change_log_folder = NULL,
-                                n_change_log = 30){
-  #If change_summary is null, then go to end
-  if(!is.null(change_summary)){
-    #If change_log_folder was not specified, then check if the default exists, if not then create it
-    if(is.null(change_log_folder)){
-      change_log_folder = paste0(load_specs()$polis_data_folder,"\\change_log")
-      if(file.exists(change_log_folder) == FALSE){
-        dir.create(change_log_folder)
-      }
-    }
-    
-    #If change_log subfolder does not exist, then create it
-    change_log_subfolder = paste0(load_specs()$polis_data_folder,"\\change_log\\", table_name)
-    if(file.exists(change_log_subfolder) == FALSE){
-      dir.create(change_log_subfolder)
-    }
-    
-    #delete the oldest file in subfolder if there are >= n_change_log files in the subfolder
-    change_log_list <- list.files(change_log_subfolder) %>%
-      stringr::str_subset(., pattern=".rds") %>%
-      stringr::str_remove(., pattern=".rds")
-    change_log_list_timestamp <- c()
-    for(j in change_log_list){
-      timestamp <- as.POSIXct(file.info(paste0(change_log_subfolder, "\\", j,".rds"))$ctime)
-      change_log_list_timestamp <- as.POSIXct(c(change_log_list_timestamp, timestamp), origin=lubridate::origin)
-    }
-    if(length(change_log_list) > 0){
-    oldest_file <- (bind_cols(file=change_log_list, timestamp=change_log_list_timestamp) %>%
-                      mutate(timestamp   = as.POSIXct(timestamp)) %>%
-                      arrange(timestamp) %>%
-                      slice(1))$file %>%
-      paste0(., ".rds")
-    }
-    if(length(change_log_list) >= n_change_log){
-      file.remove(paste0(change_log_subfolder, "\\", oldest_file))
-    }
-    #write the current file to the archive subfolder
-    write_rds(change_summary, paste0(change_log_subfolder, "\\", table_name, "_change_log_", format(as.POSIXct(Sys.time()), "%Y%m%d_%H%M%S_"),".rds"))
-  }
-
 }
 
 cleaning_var_class_from_metadata <- function(){

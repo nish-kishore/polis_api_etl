b9e9ca3f7b5c2b1d2a7c96cddc0b01792cb5e1bb
Luke Baertlein <luke.baertlein.oldsmumn@gmail.com>
2022-07-19 14:54:33 -0400

    replaced find_and_remove_deleted_obs with code instead of function
    function was short and only used once - using the code alone is more readable than using the function

--- R/process_and_call_urls_L2.R
+++ R/process_and_call_urls_L2.R
@@ -101,9 +101,12 @@ append_and_save <- function(query_output = query_output,
   
   #remove records that are no longer in the POLIS table from query_output
   if(nrow(full_idvars_output) > 0){
-    query_output <- find_and_remove_deleted_obs(full_idvars_output = full_idvars_output,
-                                                new_complete_file = query_output,
-                                                id_vars = id_vars)
+    query_output_idvars <- query_output %>%
+      select(id_vars)
+    deleted_obs <- query_output_idvars %>%
+      anti_join(full_idvars_output, by=id_vars)
+    query_output <- query_output %>%
+      anti_join(deleted_obs, by=id_vars)
   }
   #If the newly pulled dataset has any data, then read in the old file, remove rows from the old file that are in the new file, then bind the new file and old file
   if(!is.null(query_output) & nrow(query_output) > 0 & file.exists(file.path(load_specs()$polis_data_folder, paste0(table_name, ".rds")))){
@@ -158,20 +161,6 @@ append_and_save <- function(query_output = query_output,
       warning("Table is incomplete: check id_vars and field_name")
     }
   }
-}
-
-#function to remove the obs deleted from the POLIS table from the saved table
-find_and_remove_deleted_obs <- function(full_idvars_output,
-                                        new_complete_file,
-                                        id_vars){
-  id_vars <- as.vector(id_vars)
-  new_complete_file_idvars <- new_complete_file %>%
-    select(id_vars)
-  deleted_obs <- new_complete_file_idvars %>%
-    anti_join(full_idvars_output, by=id_vars)
-  new_complete_file <- new_complete_file %>%
-    anti_join(deleted_obs, by=id_vars)
-  return(new_complete_file)
 }
 
 #combine calling urls and processing output into a single function

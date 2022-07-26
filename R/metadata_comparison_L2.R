8df2483d9462e7dfe24e9a902b9963851bb05b76
Luke Baertlein <luke.baertlein.oldsmumn@gmail.com>
2022-07-26 14:34:01 -0400

    debug of metadata comparison

--- R/metadata_comparison_L2.R
+++ R/metadata_comparison_L2.R
@@ -4,10 +4,10 @@ compare_final_to_archive <- function(table_name = load_query_parameters()$table_
   id_vars <- as.vector(id_vars)
   #Load new_file
   new_file <- readRDS(file.path(load_specs()$polis_data_folder, paste0(table_name, ".rds")))
-  
+
   #load latest file in archive subfolder
   archive_subfolder <- file.path(load_specs()$polis_data_folder, "archive", table_name)
-  
+
   #for each item in subfolder_list, get all file names then subset to most recent
   subfolder_files <- list.files(paste0(archive_subfolder))
   file_dates <- c()
@@ -26,7 +26,7 @@ compare_final_to_archive <- function(table_name = load_query_parameters()$table_
   if(length(latest_file) > 0){
     #load latest_file
     latest_file <- readRDS(file.path(archive_subfolder, latest_file))
-    
+
     #get metadata for latest file and new_file
     new_file_metadata <- get_polis_metadata(query_output = new_file,
                                             table_name = table_name,
@@ -34,23 +34,27 @@ compare_final_to_archive <- function(table_name = load_query_parameters()$table_
     old_file_metadata <- get_polis_metadata(query_output = latest_file,
                                             table_name = table_name,
                                             categorical_max = categorical_max)
-    
+
     change_summary <- metadata_comparison(new_file_metadata, old_file_metadata)[2:5]
-    
+
     #count obs added to new_file and get set
     in_new_not_old <- new_file %>%
       anti_join(latest_file, by=as.vector(id_vars))
-    
+
     #count obs removed from old_file and get set
     in_old_not_new <- latest_file %>%
       anti_join(new_file, by=as.vector(id_vars))
-    
+
     #count obs modified in new file compared to old and get set
     in_new_and_old_but_modified <- new_file %>%
-      unique() %>%
+      group_by(all_of(id_vars)) %>%
+      slice(1) %>%
+      ungroup() %>%
       select(-c(setdiff(colnames(new_file), colnames(latest_file)))) %>%
       inner_join(latest_file %>%
-                   unique() %>%
+                   group_by(all_of(id_vars)) %>%
+                   slice(1) %>%
+                   ungroup() %>%
                    select(-c(setdiff(colnames(latest_file), colnames(new_file)))), by=as.vector(id_vars)) %>%
       #wide_to_long
       pivot_longer(cols=-id_vars) %>%
@@ -59,7 +63,7 @@ compare_final_to_archive <- function(table_name = load_query_parameters()$table_
       #long_to_wide
       pivot_wider(names_from=source, values_from=value) %>%
       filter(new != old)
-    
+
     #summary counts
     n_added <- nrow(in_new_not_old)
     n_edited <- nrow(in_new_and_old_but_modified %>%
@@ -67,23 +71,23 @@ compare_final_to_archive <- function(table_name = load_query_parameters()$table_
                        unique())
     n_deleted <- nrow(in_old_not_new)
     obs_change <- c(n_added = n_added,
-                    n_edited = n_edited, 
+                    n_edited = n_edited,
                     n_deleted = n_deleted)
-    
+
     #add summary to change_summary along with datasets
-    change_summary <- append(change_summary, 
-                             list(obs_change = obs_change, 
+    change_summary <- append(change_summary,
+                             list(obs_change = obs_change,
                                   obs_added = in_new_not_old,
                                   obs_edited = in_new_and_old_but_modified,
                                   obs_deleted = in_old_not_new))
-    
+
   }
   return(change_summary)
 }
 
 
 
-save_change_summary <- function(table_name = load_query_parameters()$table_name, 
+save_change_summary <- function(table_name = load_query_parameters()$table_name,
                                 change_summary,
                                 change_log_folder = NULL,
                                 n_change_log = 30){
@@ -94,13 +98,13 @@ save_change_summary <- function(table_name = load_query_parameters()$table_name,
       dir.create(change_log_folder)
     }
   }
-  
+
   #If change_log subfolder does not exist, then create it
   change_log_subfolder = file.path(load_specs()$polis_data_folder,"change_log", table_name)
   if(file.exists(change_log_subfolder) == FALSE){
     dir.create(change_log_subfolder)
   }
-  
+
   #delete the oldest file in subfolder if there are >= n_change_log files in the subfolder
   change_log_list <- list.files(change_log_subfolder) %>%
     stringr::str_subset(., pattern=".rds") %>%
@@ -145,7 +149,7 @@ print_latest_change_log_summary <- function(){
       change_log_list <- list.files(change_log_subfolder) %>%
         stringr::str_subset(., pattern=".rds") %>%
         stringr::str_remove(., pattern=".rds")
-      change_log_list_timestamp <- c()  
+      change_log_list_timestamp <- c()
       for(j in change_log_list){
         timestamp <- as.POSIXct(file.info(file.path(change_log_subfolder, paste0(j,".rds")))$ctime)
         change_log_list_timestamp <- as.POSIXct(c(change_log_list_timestamp, timestamp), origin=lubridate::origin)
@@ -173,12 +177,12 @@ print_latest_change_log_summary <- function(){
         if(length(change_log$new_vars) == 0){
           new_vars <- c()
         }
-        obs_change <- as.data.frame(change_log$obs_change) 
+        obs_change <- as.data.frame(change_log$obs_change)
         obs_change$change <- rownames(obs_change)
         obs_change <- obs_change %>%
           pivot_wider(names_from = change, values_from=`change_log$obs_change`) %>%
           mutate(table_name = i)
-        
+
         obs_change_combined <- obs_change_combined %>%
           bind_rows(obs_change)
         class_changed_vars_combined <- class_changed_vars_combined %>%
@@ -200,28 +204,28 @@ print_latest_change_log_summary <- function(){
       ungroup() %>%
       filter(tot_change > 0) %>%
       select(table_name, n_added, n_edited, n_deleted)
-    
-    #print summary of each type of change 
+
+    #print summary of each type of change
     if(nrow(new_vars_combined) > 0){
       print("New variables were found in the following tables:")
       new_vars_combined
     } else {print("No new variables were found in any table since last download.")}
-    
+
     if(nrow(lost_vars_combined) > 0){
       print("Variables were dropped from the following tables:")
       lost_vars_combined
     } else {print("No variables were dropped from any table since last download.")}
-    
+
     if(nrow(new_response_combined) > 0){
       print("New categorical responses were found in the following tables/variables:")
       new_response_combined %>% select(table_name, var_name, in_new_not_old)
     } else {print("No new categorical responses were found in any table since last download.")}
-    
+
     if(nrow(class_changed_vars_combined) > 0){
       print("Changes in variable classes were found in the following tables:")
       class_changed_vars_combined %>% select(table_name, var_name, old_var_class, new_var_class)
     } else {print("No changes in variable classes were found in any table since last download.")}
-    
+
     if(nrow(obs_change_combined) > 0){
       print("The following counts of observations were added/edited/deleted from each table since the last download:")
       obs_change_combined
@@ -238,7 +242,7 @@ get_polis_metadata <- function(query_output,
       select(skim_type, skim_variable, character.n_unique) %>%
       rename(var_name = skim_variable,
              var_class = skim_type)
-    
+
     #categorical sets: for categorical variables with <= n unique values, get a list of unique values
     categorical_vars <- query_output %>%
       select(var_name_class$var_name[var_name_class$character.n_unique <= categorical_max]) %>%
@@ -246,7 +250,7 @@ get_polis_metadata <- function(query_output,
       distinct() %>%
       pivot_wider(names_from=var_name, values_from=response, values_fn = list) %>%
       pivot_longer(cols=everything(), names_to="var_name", values_to="categorical_response_set")
-    
+
     #Combine var names/classes/categorical-sets into a 'metadata table'
     table_metadata <- var_name_class %>%
       select(-character.n_unique) %>%
@@ -268,46 +272,46 @@ metadata_comparison <- function(new_table_metadata,
       #compare to old metadata
       compare_metadata <- old_table_metadata %>%
         full_join(new_table_metadata, by=c("var_name"))
-      
+
       #Get list of new variables (variables in the new dataset but not the old)
       new_vars <- (compare_metadata %>%
                      filter(is.na(var_class.x)))$var_name
-      
+
       if(length(new_vars) != 0){
         new_vars <- new_vars
         if(verbose == TRUE){
           warning("There are new variables in the POLIS table\ncompared to when it was last retrieved\nReview in 'new_vars'")
         }
       }
-      
+
       #Get list of lost variables (variables in the old dataset but not the new)
       lost_vars <- (compare_metadata %>%
                       filter(is.na(var_class.y)))$var_name
-      
+
       if(length(lost_vars) != 0){
         lost_vars <- lost_vars
         if(verbose == TRUE){
           warning("There are missing variables in the POLIS table\ncompared to when it was last retrieved\nReview in 'lost_vars'")
         }
       }
-      
+
       #Get list of variables that have a different class in the new dataset than in old dataset
       class_changed_vars <- compare_metadata %>%
         filter(!(var_name %in% lost_vars) &
                  !(var_name %in% new_vars) &
-                 (var_class.x != var_class.y & 
+                 (var_class.x != var_class.y &
                     !is.null(var_class.x) & !is.null(var_class.y))) %>%
         select(-c(categorical_response_set.x, categorical_response_set.y)) %>%
         rename(old_var_class = var_class.x,
                new_var_class = var_class.y)
-      
+
       if(nrow(class_changed_vars) != 0){
         class_changed_vars <- class_changed_vars
         if(verbose == TRUE){
           warning("There are variables in the POLIS table with different classes\ncompared to when it was last retrieved\nReview in 'class_changed_vars'")
         }
       }
-      
+
       #Check for new responses in categorical variables (excluding new variables and class changed variables that have been previously shown)
       new_response <- compare_metadata %>%
         filter(!(var_name %in% lost_vars) &
@@ -323,15 +327,15 @@ metadata_comparison <- function(new_table_metadata,
         rename(old_categorical_response_set = categorical_response_set.x,
                new_categorical_response_set = categorical_response_set.y) %>%
         select(var_name, old_categorical_response_set, new_categorical_response_set, same, in_old_not_new, in_new_not_old)
-      
+
       if(nrow(new_response) != 0){
         new_response <- new_response
-        
+
         if(verbose == TRUE){
           warning("There are categorical responses in the new table\nthat were not seen when it was last retrieved\nReview in 'new_response'")
         }
       }
-      
+
       #Create an indicator that is TRUE if there has been a change in table structure or content that requires re-pulling of the table
       re_pull_polis_indicator <- FALSE
       if(
